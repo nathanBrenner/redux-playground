170c42a415fc3e6cbcb2338920e54597edf872d8
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-03-14 16:22:55 -0700

    toggle todos

--- src/Store.js
+++ src/Store.js
@@ -29,7 +29,7 @@ const todos = (state = [], action) => {
     case "ADD_TODO":
       return [...state, todo(undefined, action)];
     case "TOGGLE_TODO":
-      return this.state.map(t => todo(t, action));
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
 import { store, Actions } from "./Store";
+import "./App.css";
 
 let nextTodoId = 0;
 class TodoApp extends Component {
   render() {
     return (
-      <div>
+      <div class="container">
+        <input
+          ref={node => {
+            this.input = node;
+          }}
+        />
         <button
           onClick={() => {
             store.dispatch({
               type: Actions.ADD_TODO,
-              text: "Test",
+              text: this.input.value,
               id: nextTodoId++
             });
+            this.input.value = "";
           }}
         >
           Add Todo
         </button>
         <ul>
-          {this.props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+          {this.props.todos.map(todo => (
+            <li
+              key={todo.id}
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none"
+              }}
+              onClick={() => {
+                store.dispatch({
+                  type: Actions.TOGGLE_TODO,
+                  id: todo.id,
+                  text: todo.text,
+                  completed: todo.completed
+                });
+              }}
+            >
+              {todo.text}
+            </li>
+          ))}
         </ul>
       </div>
     );
