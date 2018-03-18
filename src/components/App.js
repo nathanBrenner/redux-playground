36eafb56f8f7b6579549911da640be9a96d40544
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-03-18 13:27:31 -0700

    Switched to Router's Link

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 import AddTodo from "./AddTodo";
 import VisibleTodoList from "./VisibleTodoList";
 
-const App = () => (
+const App = ({ match }) => (
   <div>
     <AddTodo />
     <VisibleTodoList />
