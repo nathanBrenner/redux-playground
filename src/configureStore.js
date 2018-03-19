2debdf537ac5a6439cf046813431e7b8431e0f6a
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-03-18 19:11:21 -0700

    wrapping dispatch to log actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,24 @@ import throttle from "lodash/throttle";
 import todoApp from "./reducers";
 import { createStore } from "redux";
 
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch;
+
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return action => {
+    console.group(action.type);
+    console.log("%c prev state", "color: gray", store.getState());
+    console.log("%c action", "color: blue", action);
+    const returnValue = rawDispatch(action);
+    console.log("%c new state", "color: green", store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(
@@ -10,6 +28,10 @@ const configureStore = () => {
     persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  if (process.env.NODE_ENV !== "production") {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(
     throttle(() => {
